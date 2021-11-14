fe3bea0285eca4d58a5b3db5fe2727243a36ff0b
t3db0t <t3db0t@gmail.com>
2021-11-14 17:28:55 -0500

    Logging what devices are detected

--- App.js
+++ App.js
@@ -7,26 +7,30 @@ export const manager = new BleManager();
 
 export default function App() {
   const scanAndConnect = () => {
+    console.log("scanAndConnect");
     manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
           // Handle error (scanning will be stopped automatically)
           return
       }
 
+      console.log("Found",device.name, device.localName);
+
       // Check if it is a device you are looking for based on advertisement data
       // or other criteria.
-      if (device.name === 'TI BLE Sensor Tag' || 
-          device.name === 'SensorTag') {
+      // if (device.name === 'TI BLE Sensor Tag' || 
+      //     device.name === 'SensorTag') {
           
-          // Stop scanning as it's not necessary if you are scanning for one device.
-          manager.stopDeviceScan();
+      //     // Stop scanning as it's not necessary if you are scanning for one device.
+      //     manager.stopDeviceScan();
 
-          // Proceed with connection.
-      }
-  });
+      //     // Proceed with connection.
+      // }
+    });
   }
 
   React.useEffect(() => {
+    console.log("Setting up initial manager subscription");
     manager.onStateChange((state) => {
       const subscription = manager.onStateChange((state) => {
           if (state === 'PoweredOn') {
